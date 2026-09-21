fd7cddfbd2982ce5a2d0eeee3b5a13e9524bddd0
abu-hanif-121285 <wiyantoa65@gmail.com>
2026-09-21 10:22:24 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
+"/indikator-keheningan-dan-timer-ujian/",
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
