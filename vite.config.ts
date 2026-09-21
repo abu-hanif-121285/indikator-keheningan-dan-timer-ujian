05caa737718c3a3b0b170dc56ed4ee883836a3b5
abu-hanif-121285 <wiyantoa65@gmail.com>
2026-09-21 10:48:29 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
-base: "/indikator-keheningan-dan-timer-ujian/",
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,6 +11,7 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteSingleFile()],
+  base: "/indikator-keheningan-dan-timer-ujian/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
